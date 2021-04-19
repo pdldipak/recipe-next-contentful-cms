f2bfc2e6db5c9a31ceb6a9cafea9f291a5c748f6
Dipak Poudel <pdl.dipak@gmail.com>
2021-04-19 12:03:45 +0200

    fixed padding

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
         <div className='flex items-center mx-auto my-6 w-full'>
           <Header />
         </div>
-        <div className='flex-grow mx-auto mt-4 mb-12 w-full px-4 py-auto'>
+        <div className='flex-grow mx-auto mt-4 mb-12 w-full px-10 py-auto'>
           <Component {...pageProps} />
         </div>
         <div className='flex justify-center w-full my-auto mx-auto p-4 bg-gray-800 '>
